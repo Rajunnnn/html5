954d6b3dc2bb38e47b1236c9cf6cbe462fad637c
Rajunnnn <joe3571599@gmail.com>
2025-06-26 15:36:24 +0900

    gps

--- humortube/src/App.jsx
+++ humortube/src/App.jsx
@@ -5,16 +5,18 @@ function App() {
   const apple = 1
 
   return (
-     <div>
+    <div className="center_div">
+
+
       <h3>유머튜브</h3>
       <iframe width="560" height="315" src="https://www.youtube.com/embed/Le1JPvEcUuQ?si=808dGQownCkoaXM5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-      
-<iframe width="560" height="315" src="https://www.youtube.com/embed/hn5aGkcEOrg?si=JRHbyQyhwX-EEjvp" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-      
+
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/hn5aGkcEOrg?si=JRHbyQyhwX-EEjvp" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
 
       <iframe width="560" height="315" src="https://www.youtube.com/embed/20twZrcympQ?si=2SojJDs0eVk8RAlK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-      
-      </div>
+
+    </div>
 
 
   )
